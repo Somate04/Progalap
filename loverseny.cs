29da8f7e414be2668e01da1d7f0a31b085cd9b4f
Somate04 <114211907+Somate04@users.noreply.github.com>
2022-11-17 17:09:24 +0100

    Add files via upload

---
+++ loverseny.cs
@@ -0,0 +1,35 @@
+﻿namespace loverseny
+{
+    internal class Program
+    {
+        static int n;
+        static int[] nyeremeny;
+
+        static int osszeg(ref int i)
+        {
+            int s = 0;
+            for(int j = 0; j <= i; j++)
+            {
+                s += nyeremeny[j];
+            }
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            nyeremeny = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                nyeremeny[i] = int.Parse(Console.ReadLine());
+            }
+
+            int j = 0;
+            while(j < n && osszeg(ref j) >= 0)
+            {
+                j++;
+            }
+            int ind = j+1;
+            Console.WriteLine(ind);
+        }
+    }
+}
